7ca625ea374c248d7e32c89a8c64c310232051d9
Carlos86-Carvalho <202510084825@alunos.estacio.br>
2026-05-26 19:37:36 +0000

    nova versão

--- war.c
+++ war.c
@@ -7,7 +7,7 @@
 #define TAM_STRING 100
 
 //Definição da Struct //
-struct Territorio{
+struct totalTerritorio{
       char Nome[TAM_STRING];
       char Cor[TAM_STRING];
       int Tropas;
@@ -16,14 +16,14 @@ struct Territorio{
 // Limpar o Buffer de entrada //
 void limparBufferEntrada(){
     int c;
-    while((c  getchar()) != 'n' && c != EOF);
+    while((c = getchar()) != '\n' && c != EOF);
 }
 
 // Função Pricipal (main) //
 int main(){
-    struct Territorio elemento[MAX_TERRITORIOS];
+    struct totalTerritorio elemento[MAX_TERRITORIOS];
     int totalTerritorio = 0;
-    int Tropas;
+    int opcao;
 
     // Menu //
 
@@ -33,54 +33,60 @@ do {
     // Exibição para usuário das opções //
     printf("=============================\n");
     printf(" Bem vindo ao Registro Total WAR 2.0\n");
-    printf("1 - Digite Nome do Território\n");
-    printf("2 - Escolha uma Cor para o Exército\n");
-    Printf("3 - Digite o Número de Tropas\n");
+    printf("1 - Cadastrar Novo Território\n");
+    printf("2 - Listar Territórios\n");
     printf("0 - Sair\n");
     printf("----------------------------------------\n");
     printf("Escolha uma Opção: ");
 
     // Opção do Usuário //
 
-    scanf("%d", &Tropas);
+    if (scanf("%d", &opcao) != 1){
+        opcao = -1;
+    }
     limparBufferEntrada();
 
     //Processamento das Tropas//
-switch (Tropas) {
+switch (opcao) {
     case 1: //Cadastro de Território//
-       printf("===Cadastro de Nova Tropa===\n\n");
+       printf("===Cadastro de Novo Território===\n\n");
 
-       if (TotalTropas < MAX_TERRITORIOS){
+       if (totalTerritorio < MAX_TERRITORIOS){
           printf("Digite o Nome do Território: ");
-          fgets(Territorio[totalTerritotio].Nome, TAM_STRING, stdin);
+          fgets(elemento[totalTerritorio].Nome, TAM_STRING, stdin);
+          elemento[totalTerritorio].Nome[strcspn(elemento[totalTerritorio].Nome, "\n")] = 0;
 
-          printf("Digite a Cor do território: ");
-          fgets(Territorio[totalCor].Cor, TAM_STRING, stdin);
+          printf("Digite a Cor do exército: ");
+          fgets(elemento[totalTerritorio].Cor, TAM_STRING, stdin);
+          elemento[totalTerritorio].Cor[strcspn(elemento[totalTerritorio].Cor, "\n")] = 0;
 
-          printf("Digite numero de tropas: ");
-          scanf("%d", &Territorio[totalTerritotio].Tropas);
+          printf("Digite número de tropas: ");
+          scanf("%d", &elemento[totalTerritorio].Tropas);
           limparBufferEntrada();
 
-          totalterritorios++;
+          totalTerritorio++;
 
           printf("\nTerritorio cadastrado com sucesso!\n");
         }else{
-            printf("Limete de Registros alcançado!. \n");
+            printf("Registros Cheios! Não há mais espaço. \n");
 
         }
-         printf("\nPrecione Enter para continuar");
+         printf("\nPressione Enter para continuar");
          getchar();
          break;
 
-    case 2: Lista de territorio
-         printf("Nenhum território cadastrado ainda. \n");
-    } else {
+    case 2: //Lista de territórios//
+         printf("\n===Territórios Cadastrados === \n\n");
+         if (totalTerritorio == 0) {
+            printf("Nenhum territorio cadastrado ainda.\n");
+
+         } else {
         for (int i = 0; i < totalTerritorio; i++){
             printf("--------------------------------------\n");
             printf("Território %d\n", i + 1);
-            printf("Nome: %s\n", Territorio[i].Nome);
-            printf("Cor: %s\n", Territorio[i].Cor);
-            printf("Tropas: %d", Territorio[i].Tropas);
+            printf("Nome: %s\n", elemento[i].Nome);
+            printf("Cor: %s\n", elemento[i].Cor);
+            printf("Tropas: %d", elemento[i].Tropas);
         }
 
         printf("----------------------------------------\n");
@@ -95,10 +101,13 @@ switch (Tropas) {
     
     default: //Se existirem Opções inválidas //
         printf("\nOpção Inválida! Tente novamente.\n");
-        printf("\nPrecione Enter para Continuar...\n");
+        printf("\nPressione Enter para Continuar...\n");
         getchar();
         break;
     }     
-    }
-    return 0
-
+    while (opcao !=0); 
+    
+}
+return 0;
+} while (opcao =0);
+}
